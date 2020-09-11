24b75baa87abf6890168ce36de3cf8753def5ba5
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-09-10 20:58:53 -0400

    Update examples.hs

--- examples.hs
+++ examples.hs
@@ -6,3 +6,5 @@ average = starling' divide sum length
 
 palindrome = starling (++) (reverse . init)        -- version 1
 palindrome = starling (++) (bluebird reverse init) -- version 2
+
+round_robin_padding = goldfinch take cycle -- https://godbolt.org/z/MdTPEs
